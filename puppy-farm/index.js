4b8480c460d4c6d4da1c90f58a56cdb970c0ecb7
Mr Whipp <dwhipp88@gmail.com>
2022-04-17 01:39:29 -0600

    created a json response in server

--- puppy-farm/index.js
+++ puppy-farm/index.js
@@ -1,10 +1,19 @@
 const http = require("http");
 const url = require("url");
+const fs = require("fs");
 
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+// const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObj = JSON.parse(data);
+// console.log(data);
 // *** Server ***
 const server = http.createServer((req, res) => {
-  res.writeHead(200);
-  res.end("Up & Running!");
+  // basic setup to ensure server is running
+  res.writeHead(200, {
+    "Content-type": "application/json",
+  });
+  res.end(data);
+  //   console.log(data);
 });
 server.listen(5500, "127.0.0.1", () => {
   console.log("Listening to requests on port 5500");
