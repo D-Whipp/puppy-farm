9788c3f71fb103d29607af17e7e54ca30273fdf3
Mr Whipp <dwhipp88@gmail.com>
2022-04-17 23:49:36 -0600

    templates/ - created template card and overview | integrated their data to appear browser when running index.js

--- puppy-farm/index.js
+++ puppy-farm/index.js
@@ -1,18 +1,45 @@
 const http = require("http");
 const url = require("url");
 const fs = require("fs");
+const replaceTemplate = require("./modules/replaceTemplate");
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 // const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
+const templateOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  "utf-8"
+);
+const templateCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  "utf-8"
+);
 // console.log(data);
 // *** Server ***
 const server = http.createServer((req, res) => {
-  // basic setup to ensure server is running
-  res.writeHead(200, {
-    "Content-type": "application/json",
-  });
-  res.end(data);
+  const { query, pathname } = url.parse(req.url, true);
+
+  // Overview page
+  if (pathname === "/" || pathname === "/overview") {
+    // basic setup to ensure server is running
+    res.writeHead(200, {
+      "Content-type": "application/json",
+    });
+
+    const cardsHtml = dataObj
+      .map((el) => replaceTemplate(templateCard, el))
+      .join("");
+    const output = templateOverview.replace("{%PRODUCT_CARDS%}", cardsHtml);
+    res.end(output);
+  } else {
+    // Not Found
+    res.writeHead(404, {
+      "Content-type": "text/html",
+    });
+    res.end("<h1>Page not found!</h1>");
+  }
+
+  // res.end(data);
   //   console.log(data);
 });
 server.listen(5500, "127.0.0.1", () => {
